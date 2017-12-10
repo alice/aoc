d82ac6ba0ddd4737c0cee6870c1e6d8ae4e58b6f
Alice Boxhall <aboxhall@chromium.org>
2017-12-10 18:46:41 +1100

    Level 7 WIP

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,53 @@
-use std::collections::HashMap;
+extern crate regex;
+
+use regex::Regex;
+use std::collections::HashSet;
+use std::hash::{Hash, Hasher};
 use std::io::{self, Read};
 
-fn level6() {
-    let mut input = String::new();
+#[derive(Default)]
+struct Program<'a> {
+    name: &'a str,
+    weight: i32,
+    supporting: HashSet<&'a Program<'a>>,
+    supported_by: Option<&'a Program<'a>>,
+}
 
+impl<'a> PartialEq for Program<'a> {
+    fn eq(&self, other: &Program<'a>) -> bool {
+        return self.name == other.name;
+    }
+}
+
+impl<'a> Eq for Program<'a> {}
+
+impl<'a> Hash for Program<'a> {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.name.hash(state);
+    }
+}
+
+fn level7() {
+    let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
 
-    let mut banks: Vec<i32> = input
-        .trim()
-        .split_whitespace()
-        .map(|x| {
-            x.parse().expect(
-                format!("could not parse {:?}", x).as_str(),
-            )
-        })
-        .collect();
-    let mut steps = 0;
-    let mut seen: HashMap<Vec<i32>, i32> = HashMap::new();
-    while !seen.contains_key(&banks) {
-        seen.insert(banks.clone(), steps);
-        let mut max = 0;
-        let mut max_at = 0;
-        banks.iter().enumerate().for_each(
-            |(i, &bank)| if bank > max {
-                max = bank;
-                max_at = i;
-            },
-        );
-        banks[max_at] = 0;
-        let mut remaining = max;
-        let mut i = (max_at + 1) % banks.len();
-        while remaining > 0 {
-            banks[i] += 1;
-            remaining -= 1;
-            i = (i + 1) % banks.len();
-        }
-        steps += 1;
+    let lines = input.trim().split("\n");
+    let re = Regex::new(r"(\w+) \((\d+)\)(:? -> )?((:?\w+)(:?, )?)*").unwrap();
+    for line in lines {
+        println!("line: {:?}", line);
+        let captures = re.captures(line).unwrap();
+        let name: &str = &captures[1];
+        let weight: i32 = captures[2].parse().unwrap();
+        let program: Program = Program {
+            name,
+            weight,
+            ..Default::default()
+        };
+        println!("captures: {:?}", captures);
+        println!("name: {:?}, weight: {:?}, supports: {:?}", name, weight, 0)
     }
-
-    println!("loop size: {}", steps - seen.get(&banks).unwrap());
 }
 
 fn main() {
-    level6();
+    level7();
 }
