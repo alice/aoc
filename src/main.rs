9528c4bae0e791bb1e0579a662246a35576cf62d
Alice Boxhall <aboxhall@chromium.org>
2017-12-18 16:44:51 +1100

    Finished day 16

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,27 @@
 use std::char;
+use std::collections::HashMap;
 use std::io::{self, Read};
 
 trait Dance {
-    fn spin(&mut self, args: &str);
-    fn exchange(&mut self, args: &str);
-    fn swap(&mut self, a: usize, b: usize);
-    fn partner(&mut self, args: &str);
+    fn spin(&mut self, x: usize);
+    fn exchange(&mut self, a: usize, b: usize);
+    fn partner(&mut self, a: char, b: char);
 }
 
 impl Dance for String {
-    fn spin(&mut self, args: &str) {
-        let x: usize = args.parse().unwrap();
+    fn spin(&mut self, x: usize) {
         let end = self.len();
         let split = self.split_off(end - x);
         self.insert_str(0, split.as_str());
     }
 
-    fn exchange(&mut self, args: &str) {
-        let mut split = args.split("/");
-        let a: usize = split.next().unwrap().parse().unwrap();
-        let b: usize = split.next().unwrap().parse().unwrap();
-        self.swap(a, b);
-    }
-
-    fn swap(&mut self, a: usize, b: usize) {
+    fn exchange(&mut self, a: usize, b: usize) {
         unsafe {
             self.as_mut_vec().swap(a, b);
         }
     }
 
-    fn partner(&mut self, args: &str) {
-        let mut split = args.split("/");
-        let a = split.next().unwrap().chars().next().unwrap();
-        let b = split.next().unwrap().chars().next().unwrap();
+    fn partner(&mut self, a: char, b: char) {
         let mut a_idx: usize = 0;
         let mut b_idx: usize = 0;
         let mut found_a = false;
@@ -51,8 +40,14 @@ impl Dance for String {
                 break;
             }
         }
-        self.swap(a_idx, b_idx);
+        self.exchange(a_idx, b_idx);
     }
+}
+
+enum Move {
+    Spin(usize),
+    Exchange { a: usize, b: usize },
+    Partner { a: char, b: char },
 }
 
 fn level16() {
@@ -65,18 +60,58 @@ fn level16() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
     let moves = input.trim().split(",");
+    let mut compiled_moves: Vec<Move> = Vec::new();
     for mut mv in moves.map(|m| m.to_string()) {
-        println!("mv: {:?}", mv);
         let t = mv.remove(0);
-        match t {
-            's' => programs.spin(mv.as_str()),
-            'x' => programs.exchange(mv.as_str()),
-            'p' => programs.partner(mv.as_str()),
-            _ => panic!("weird type {:?}", t),
+        if t == 's' {
+            let x: usize = mv.parse().unwrap();
+            compiled_moves.push(Move::Spin(x));
+        } else if t == 'x' {
+            let mut split = mv.split("/");
+            let a: usize = split.next().unwrap().parse().unwrap();
+            let b: usize = split.next().unwrap().parse().unwrap();
+            compiled_moves.push(Move::Exchange { a, b });
+        } else if t == 'p' {
+            let mut split = mv.split("/");
+            let a = split.next().unwrap().chars().next().unwrap();
+            let b = split.next().unwrap().chars().next().unwrap();
+            compiled_moves.push(Move::Partner { a, b });
+        }
+    }
+    let num_moves = compiled_moves.len();
+    let compiled_moves = compiled_moves;
+    let mut seen: HashMap<String, u32> = HashMap::new();
+    let mut period = 0;
+    for round in 1..1000000000 {
+        for i in 0..num_moves {
+            match compiled_moves[i] {
+                Move::Spin(x) => programs.spin(x),
+                Move::Exchange { a, b } => programs.exchange(a, b),
+                Move::Partner { a, b } => programs.partner(a, b),
+            }
+        }
+        if seen.contains_key(&programs) {
+            period = round - seen.get(&programs).unwrap();
+            println!(
+                "{:?} repeated after {} rounds",
+                programs,
+                round - seen.get(&programs).unwrap()
+            );
+            break;
+        } else {
+            seen.insert(programs.clone(), round);
+        }
+        if round % 10000 == 0 {
+            println!("done {}: {:?}", round, programs);
         }
     }
 
-    println!("{:?}", programs);
+    let index = 1000000000 % period;
+    for (programs, i) in seen {
+        if i == index {
+            println!("after 1 billion runs: {:?}", programs);
+        }
+    }
 }
 
 fn main() {
